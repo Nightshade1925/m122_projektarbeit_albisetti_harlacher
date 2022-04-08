b6836dd053fadec596768267bf345e3b3ddf8dc0
Paul Albisetti <palbisetti@soxes.switzerland>
2022-04-08 10:22:12 +0200

    git extract commits

--- bin/utils.py
+++ bin/utils.py
@@ -63,3 +63,7 @@ class Utils:
 			return config
 		except Exception as e:
 			raise Exception("Failed to Load configuration file {}: {}".format(filename,e))
+
+	@staticmethod
+	def get_immediate_subdirectories(a_dir):
+		return [name for name in os.listdir(a_dir) if os.path.isdir(os.path.join(a_dir, name))]
