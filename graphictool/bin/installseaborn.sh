f04d4cbf71ff7e1916fd4d6de10efc3d3319b260
Benjamin Harlacher <pootawanbarca@hotmail.com>
2022-05-06 09:36:46 +0200

    Update installseaborn.sh

--- graphictool/bin/installseaborn.sh
+++ graphictool/bin/installseaborn.sh
@@ -3,8 +3,8 @@ if [ ! -d venv_for_seaborn ] ; then
   sudo apt-get install python3-venv
   python3 -m venv $(dirname $0)/venv_for_seaborn
   . venv_for_seaborn/bin/activate
-  pip install --upgrade pip
-  pip install --upgrade setuptools
-  pip install seaborn
+  pip3 install --upgrade pip
+  pip3 install --upgrade setuptools
+  pip3 install seaborn
 fi
 . venv_for_seaborn/bin/activate
